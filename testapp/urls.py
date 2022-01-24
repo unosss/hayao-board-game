e5f992a032e300799ce6681cca49fb4f5239bebf
Satoru Uno <unofamil@outlook.com>
2022-01-24 21:17:28 +0900

    2

--- testapp/urls.py
+++ testapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import signupview, loginview, listview, detailview, CreateClass, logoutview, evaluationview
 
 urlpatterns = [
-    path('signup/', signupview, name='signup'),
+    path('', signupview, name='signup'),
     path('login/', loginview, name='login'),
     path('list/', listview, name='list'),
     path('detail/<int:pk>/', detailview, name='detail'),
